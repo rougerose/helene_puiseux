05e443194759a94a6730c541f326d9b600bf1005
christophe le drean <christophe@rouge-rose.net>
2014-04-29 09:58:37 +0200

    base pour la version dev : dépendances via bower, node et grunt

---
+++ GruntFile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      compile: {
+        options: {
+          loadPath: [
+            'bower_components/bourbon/dist',
+            'bower_components/inuit.css'
+          ],
+          style: 'compressed',
+        },
+        files: {
+          'css/styles.css':'css/styles.scss'
+        }
+      }
+    },
+    cssmetrics: {
+      common: {
+        src: ['css/styles.css'],
+        options: {
+          quiet: false,
+          maxRules: 4096, //IE max rules
+          maxFileSize: 1048576 //1mb in bytes
+        }
+      }
+    },
+    watch: {
+      grunt: { files: ['GruntFile.js'] },
+      sass: {
+        files: 'css/**/*.scss',
+        tasks: ['sass:compile']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-css-metrics');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('analyse', ['cssmetrics:common']);
+  grunt.registerTask('compile', ['sass:compile']);
+  grunt.registerTask('default', ['compile','analyse']);
+}
