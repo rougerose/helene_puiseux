1fd4a4cc4b7c80bf7975957e25ab2f7bd8b80d33
christophe le drean <christophe@rouge-rose.net>
2014-05-14 00:37:36 +0200

    article

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,8 +2,11 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+    // I
+    'info_ps' => "Post-scriptum",
+    // P
+    "par" => "Par",
     // S
-    
     "suite" => "Lire la suite <em>&#8594;</em>"
    
    );
