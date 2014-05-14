d392998eeee39e65a978da043335bf9781043577
christophe le drean <christophe@rouge-rose.net>
2014-05-14 08:24:35 +0200

    suite article + composition article-bibliographie

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -5,6 +5,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     // I
     'info_ps' => "Post-scriptum",
     // P
+    'pager_precedent' => '&#8249;',
+    'pager_precedent_info' => 'Article précédent',
+    'pager_suivant' => '&#8250;',
+    'pager_suivant_info' => 'Article suivant',
     "par" => "Par",
     // S
     "suite" => "Lire la suite <em>&#8594;</em>"
