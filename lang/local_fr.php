57cbe97d82e34a839bd0cf9e0ea8a46892c9cdb5
christophe le drean <christophe@rouge-rose.net>
2014-05-28 12:13:22 +0200

    recherche

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,21 +2,40 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+    
     // D
     'documents' => "Documents",
+    
     // I
     'info_page' => "Page",
     'info_ps' => "Post-scriptum",
+    'info_recherche_article'         => '1 article',
+    'info_recherche_articles'        => '@nb@ articles',
+    'info_recherche_document'        => '1 document',
+    'info_recherche_documents'       => '@nb@ documents',
+    'info_recherche_label'           => 'Recherche',
+    'info_recherche_mot'             => '1 mot-clé',
+    'info_recherche_mots'            => '@nb@ mots-clés',
+    'info_recherche_rubrique'        => '1 rubrique',
+    'info_recherche_rubriques'       => '@nb@ rubriques',
+    'info_recherche_site'            => '1 site web',
+    'info_recherche_sites'           => '@nb@ sites web',
+    'info_recherche_resultat'        => '1 résultat',
+    'info_recherche_resultats'       => '@nb@ résultats',
+    'info_recherche_saisie' => "saisir votre recherche",
+    'info_recherche_pas_de_resultat' => 'Aucun résultat',
+
     // P
     'pager_precedent' => '&#8249;',
     'pager_precedent_info' => 'Précédent',
     'pager_suivant' => '&#8250;',
     'pager_suivant_info' => 'Suivant',
     "par" => "Par",
+    
     // S
     "suite" => "Lire la suite <em>&#8594;</em>",
+    
     // T
     "trier" => "Trier"
-   
    );
 ?>
