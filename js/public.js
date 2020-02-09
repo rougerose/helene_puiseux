4b2e51c9f27818d73a34d8360eceec75ab91a91b
christophe le drean <christophe@rouge-rose.net>
2020-02-09 17:43:46 +0100

    Adaptations et corrections

--- js/public.js
+++ js/public.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+
+    //
     // navigation principale
+    //
     var doc = $(document.documentElement);
     $("#nav-open").click(function(){
         doc.addClass("js-nav-open");
@@ -9,16 +12,58 @@ $(document).ready(function() {
         doc.removeClass("js-nav-open");
         return false;
     });
+
+    //
     // menu filtres
-    $("#filtres").each(function(){
-        var $menu = $(this),
-            $toggle = $menu.find($("#filtres-toggle")),
-            $nav = $menu.find($("nav"));
-        $nav.hide();
-        $toggle.click(function(){
-            $(this).toggleClass("is-active");
-            $nav.slideToggle().toggleClass("is-open");
-            return false;
+    //
+    $(".articles-liste").each(function() {
+      var $articles = $(this);
+      var $nav = $articles.find("nav.js-filtres-nav"),
+          $btn = $articles.find("#btnFiltres");
+
+      // fermer au chargement de la page
+      closeNav($nav);
+
+      // Bouton d'ouverture des filtres
+      $btn.click(function() {
+        toggleBtn($(this));
+        toggleNav($nav);
+      });
+
+      // Après rechargement du bloc en ajax,
+      // recupérer les éléments boutons et navigation de filtres
+      $(document).ajaxComplete(function(event) {
+        // console.log(event);
+        var $btn = $("#btnFiltres");
+        var $nav = $("nav.js-filtres-nav");
+
+        // la navigation des filtres est ouverte par défaut.
+        closeNav($nav);
+
+        // Actions sur le bouton
+        $btn.click(function() {
+          toggleBtn($(this));
+          toggleNav($nav);
         });
+      });
     });
+
+    function toggleBtn(el) {
+      el.toggleClass("is-active");
+    }
+
+    // function openNav(el) {
+    //   el.slideDown();
+    //   el.addClass("is-open");
+    // }
+
+    function closeNav(el) {
+      el.slideUp();
+      el.removeClass("is-open");
+    }
+
+    function toggleNav(el) {
+      el.slideToggle(300);
+      el.toggleClass("is-open");
+    }
 });
